af1584bca03506819936cd42aec90a66c0269523
tidwellchris <tidwellchris@gmail.com>
2016-03-07 13:41:12 -0500

    Update db.js

--- public/javascripts/db.js
+++ public/javascripts/db.js
@@ -1,1 +1,7 @@
 //This is where the SQL connections are added to handle the insert statements
+
+// mysql.query('insert into '+ TABLE +' (name, price) values ("' + req.body.name + '", "' + req.body.name.price + '")',
+// function selectCb(err, results, fields) {
+//     if (err) throw err;
+//     else res.send('success');
+// });
