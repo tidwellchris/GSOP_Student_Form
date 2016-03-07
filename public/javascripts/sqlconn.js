a0f7e2ad02cd8f67d2629ff5768814228ca10b36
tidwellchris <tidwellchris@gmail.com>
2016-03-07 12:15:26 -0500

    Adding SQL Connections

--- public/javascripts/sqlconn.js
+++ public/javascripts/sqlconn.js
@@ -1,1 +1,10 @@
 var mysql = require(mysql);
+
+
+
+var conn = mysql.createConnection({
+  host     : 'mysql.server266.com',
+  user     : 'dgsopb',
+  password : 'Preaching3ch00l',
+  database : 'gsoponline_sis'
+});
