e016aa3299b5cdd816aee829b501365adc3ad46d
tidwellchris <tidwellchris@gmail.com>
2016-03-09 14:43:39 -0500

    Updated the get to get form data on submit

--- app.js
+++ app.js
@@ -3,21 +3,26 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 //var favicon = require('serve-favicon');
 
 //Needed to serve up static paths in all public folder.
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Serve the html form for the users
+
+
 app.get("/",function(req, res){
   res.sendfile("public/html/form.html");
 });
 
 //get responses of the html form
 app.post('/', function(req, res){
-  var username = req.body.user;
+  var email = req.body.email;
+  console.log('This is the email: ' + email);
   var password = req.body.password;
+  console.log('This is the Password' + password);
+  console.log('This is what is being passed' + JSON.stringify(req.body));
 });
 
 app.listen(3000, function(){
